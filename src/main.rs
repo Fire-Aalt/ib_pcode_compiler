60a741c499216e783008662455275484041ac0b8
Maksim Krasutski <krasutskimaksim@gmail.com>
2025-09-21 18:31:51 +0200

    Add AST struct

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ enum Stmt {
     While(Expr, Vec<Stmt>),
     For(String, Expr, Expr, Vec<Stmt>),
     Output(Vec<Expr>),
+    Method(Vec<String>, Vec<Stmt>),
     EOI
 }
 
@@ -62,89 +63,126 @@ enum Operator {
 }
 
 
-fn build_ast(pair: pest::iterators::Pair<Rule>) -> Vec<Stmt> {
-    assert_eq!(pair.as_rule(), Rule::program);
-    pair.into_inner().map(build_stmt).collect()
+struct AST {
+    statements: Vec<Stmt>,
+    method_map: HashMap<String, method_def>,
 }
 
-fn build_stmt(pair: pest::iterators::Pair<Rule>) -> Stmt {
-    match pair.as_rule() {
-        Rule::assign => {
-            let mut inner = pair.into_inner();
-            let ident = inner.next().unwrap().as_str().to_string();
-            let expr = build_expr(inner.next().unwrap());
-            Stmt::Assign(ident, expr)
-        }
-        Rule::if_stmt => {
-            let mut inner = pair.into_inner();
-            let cond = build_expr(inner.next().unwrap());
-            let body: Vec<Stmt> = inner.map(build_stmt).collect();
-            Stmt::If(cond, body)
-        }
-        Rule::while_loop_stmt => {
-            let mut inner = pair.into_inner();
-            let cond = build_expr(inner.next().unwrap());
-            let body: Vec<Stmt> = inner.map(build_stmt).collect();
-            Stmt::While(cond, body)
-        }
-        Rule::for_loop_stmt => {
-            let mut inner = pair.into_inner();
-            let ident = inner.next().unwrap().as_str().to_string();
-            let start_num = build_expr(inner.next().unwrap());
-            let end_num = build_expr(inner.next().unwrap());
-            let body: Vec<Stmt> = inner.map(build_stmt).collect();
-            Stmt::For(ident, start_num, end_num, body)
-        }
-        Rule::output_stmt => {
-            let inner = pair.into_inner();
-            let body: Vec<Expr> = inner.map(build_expr).collect();
-            Stmt::Output(body)
-        }
-        Rule::EOI => Stmt::EOI,
-        _ => unreachable!(),
+impl Default for AST {
+    fn default() -> Self {
+        AST { statements: Vec::new(), method_map: HashMap::new() }
     }
 }
 
-fn build_expr(pair: pest::iterators::Pair<Rule>) -> Expr {
-    match pair.as_rule() {
-        Rule::expr => {
-            let mut inner = pair.into_inner();
-            let mut left = build_expr(inner.next().unwrap());
-            while let Some(op) = inner.next() {
-                let right = build_expr(inner.next().unwrap());
+impl Display for AST {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{:#?}", self.statements)
+    }
+}
 
-                let op = match op.as_rule() {
-                    Rule::add => Operator::Add,
-                    Rule::subtract => Operator::Subtract,
-                    Rule::multiply => Operator::Multiply,
-                    Rule::divide => Operator::Divide,
-                    Rule::power => Operator::Power,
-                    Rule::modulo => Operator::Modulo,
-                    Rule::greater => Operator::Greater,
-                    Rule::less => Operator::Less,
-                    Rule::greater_equal => Operator::GreaterEqual,
-                    Rule::less_equal => Operator::LessEqual,
-                    Rule::equal => Operator::Equal,
-                    Rule::not_equal => Operator::NotEqual,
-                    _ => unreachable!(),
-                };
+impl AST {
+    pub fn traverse(&self, env: &mut HashMap<String, DataType>) {
+        for stmt in &self.statements {
+            exec_stmt(stmt, env);
+        }
+    }
 
-                left = Expr::BinOp(Box::new(left), op, Box::new(right));
+    pub fn build_ast(&mut self, pair: pest::iterators::Pair<Rule>) {
+        self.method_map = HashMap::new();
+        assert_eq!(pair.as_rule(), Rule::program);
+
+        self.statements = pair.into_inner().map(|inner| self.build_stmt(inner)).collect();
+    }
+
+    fn build_stmt(&mut self, pair: pest::iterators::Pair<Rule>) -> Stmt {
+        match pair.as_rule() {
+            Rule::assign => {
+                let mut inner = pair.into_inner();
+                let ident = inner.next().unwrap().as_str().to_string();
+                let expr = self.build_expr(inner.next().unwrap());
+                Stmt::Assign(ident, expr)
             }
-            left
+            Rule::if_stmt => {
+                let mut inner = pair.into_inner();
+                let cond = self.build_expr(inner.next().unwrap());
+                let body: Vec<Stmt> = inner.map(|inner| self.build_stmt(inner)).collect();
+                Stmt::If(cond, body)
+            }
+            Rule::while_loop_stmt => {
+                let mut inner = pair.into_inner();
+                let cond = self.build_expr(inner.next().unwrap());
+                let body: Vec<Stmt> = inner.map(|inner| self.build_stmt(inner)).collect();
+                Stmt::While(cond, body)
+            }
+            Rule::for_loop_stmt => {
+                let mut inner = pair.into_inner();
+                let ident = inner.next().unwrap().as_str().to_string();
+                let start_num = self.build_expr(inner.next().unwrap());
+                let end_num = self.build_expr(inner.next().unwrap());
+                let body: Vec<Stmt> = inner.map(|inner| self.build_stmt(inner)).collect();
+                Stmt::For(ident, start_num, end_num, body)
+            }
+            Rule::output_stmt => {
+                let inner = pair.into_inner();
+                let body: Vec<Expr> = inner.map(|inner| self.build_expr(inner)).collect();
+                Stmt::Output(body)
+            }
+            Rule::method_stmt => {
+                let mut inner = pair.into_inner();
+                let method_name = inner.next().unwrap().as_str().to_string();
+
+                let body: Vec<Stmt> = inner.map(|inner| self.build_stmt(inner)).collect();
+
+                Stmt::Method(Vec::new(), body)
+            }
+            Rule::EOI => Stmt::EOI,
+            _ => unreachable!(),
         }
-        Rule::term => build_expr(pair.into_inner().next().unwrap()),
-        Rule::ident => Expr::Ident(pair.as_str().to_string()),
-        Rule::number => Expr::Data(DataType::Number(pair.as_str().parse().unwrap())),
-        Rule::string => Expr::Data(DataType::String(fix_quotes_plain(pair.as_str()))),
-        Rule::input => {
-            let mut inner = pair.into_inner();
-            let text = build_expr(inner.next().unwrap());
-            Expr::Input(Box::new(text))
-        },
-        _ => unreachable!(),
+    }
+
+    fn build_expr(&mut self, pair: pest::iterators::Pair<Rule>) -> Expr {
+        match pair.as_rule() {
+            Rule::expr => {
+                let mut inner = pair.into_inner();
+                let mut left = self.build_expr(inner.next().unwrap());
+                while let Some(op) = inner.next() {
+                    let right = self.build_expr(inner.next().unwrap());
+
+                    let op = match op.as_rule() {
+                        Rule::add => Operator::Add,
+                        Rule::subtract => Operator::Subtract,
+                        Rule::multiply => Operator::Multiply,
+                        Rule::divide => Operator::Divide,
+                        Rule::power => Operator::Power,
+                        Rule::modulo => Operator::Modulo,
+                        Rule::greater => Operator::Greater,
+                        Rule::less => Operator::Less,
+                        Rule::greater_equal => Operator::GreaterEqual,
+                        Rule::less_equal => Operator::LessEqual,
+                        Rule::equal => Operator::Equal,
+                        Rule::not_equal => Operator::NotEqual,
+                        _ => unreachable!(),
+                    };
+
+                    left = Expr::BinOp(Box::new(left), op, Box::new(right));
+                }
+                left
+            }
+            Rule::term => self.build_expr(pair.into_inner().next().unwrap()),
+            Rule::ident => Expr::Ident(pair.as_str().to_string()),
+            Rule::number => Expr::Data(DataType::Number(pair.as_str().parse().unwrap())),
+            Rule::string => Expr::Data(DataType::String(fix_quotes_plain(pair.as_str()))),
+            Rule::input => {
+                let mut inner = pair.into_inner();
+                let text = self.build_expr(inner.next().unwrap());
+                Expr::Input(Box::new(text))
+            },
+            _ => unreachable!(),
+        }
     }
 }
+
+
 
 fn eval_expr(expr: &Expr, env: &HashMap<String, DataType>) -> DataType {
     match expr {
@@ -159,10 +197,11 @@ fn eval_expr(expr: &Expr, env: &HashMap<String, DataType>) -> DataType {
 
             let mut input = String::new();
             io::stdin().read_line(&mut input).unwrap();
+            let input = input.trim();
 
-            match input.trim().parse::<f64>() {
+            match input.parse::<f64>() {
                 Ok(number) => DataType::Number(number),
-                Err(_) => DataType::String(input)
+                Err(_) => DataType::String(input.to_string())
             }
         }
         Expr::BinOp(left, op, right) => {
@@ -274,6 +313,7 @@ fn exec_stmt(stmt: &Stmt, env: &mut HashMap<String, DataType>) {
             println!("{}", output);
         }
         Stmt::EOI => {},
+        &Stmt::Method(_, _) => todo!(),
     }
 }
 
@@ -327,7 +367,7 @@ fn main() {
     "#;
 
     let ast = compile(code);
-    println!("{:#?}", ast);
+    println!("{}", ast);
 
     let env = run(ast);
     println!("Final env: {:?}", env);
@@ -335,21 +375,29 @@ fn main() {
 
 
 
-fn compile(code: &str) -> Vec<Stmt> {
+fn compile(code: &str) -> AST {
     let parsed = DSLParser::parse(Rule::program, code)
         .expect("parse failed")
         .next()
         .unwrap();
 
-    build_ast(parsed)
+    let mut ast = AST::default();
+    ast.build_ast(parsed);
+    ast
 }
 
-fn run(ast: Vec<Stmt>) -> HashMap<String, DataType> {
-    let mut env = HashMap::new();
 
-    for stmt in &ast {
-        exec_stmt(stmt, &mut env);
-    }
+
+#[derive(Clone)]
+struct method_def {
+    pub name: String,
+    pub args: Vec<Expr>,
+    pub body: Vec<Stmt>,
+}
+
+fn run(ast: AST) -> HashMap<String, DataType> {
+    let mut env = HashMap::new();
+    ast.traverse(&mut env);
     env
 }
 
